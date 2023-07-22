afe9908e635931976642fdde2fcafc2d55dcd876
Kutbidinov Sherzod <skutbidiov1@gmail.com>
2023-07-22 18:03:04 +0200

    started developing a filter

--- src/components/productsInCategoriesModule/all_products/All_products.jsx
+++ src/components/productsInCategoriesModule/all_products/All_products.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import styles from './all_products.module.css'
 import secateurs from '../../../assets/productsInCategories/secateurs.png'
+import ViewProducts from './viewProducts/ViewProducts';
+import SortProducts from './sortProducts/SortProducts';
+import FilterProducts from './filterProducts/FilterProducts';
 
 
 export default function All_products() {
@@ -98,6 +101,11 @@ export default function All_products() {
     }
   }
 
+  const filterProducts = (priceFrom, priceTo) => {
+    products.filter((product) => {
+      return setProducts(product.newPrice > priceFrom && product.newPrice < priceTo)
+    })
+  }
 
 
 
@@ -107,11 +115,7 @@ export default function All_products() {
 
       <div className={styles.form_container}>
 
-        <div className={styles.price_container}>
-          <span className={styles.text}>Price</span>
-          <input placeholder='from' className={styles.price_input}></input>
-          <input placeholder='to' className={styles.price_input}></input>
-        </div>
+        <FilterProducts filterProducts={filterProducts} />
 
         <div className={styles.discount_container}>
           <span className={styles.text}>Discounted items</span>
@@ -123,174 +127,16 @@ export default function All_products() {
         <div className={styles.sort_container}>
           <span className={styles.text}>Sorted</span>
           <span>
-            <select onChange={event => sortProducts(event)} className={styles.sort_select}>
-              <option className={styles.sort_option} value="default">By default</option>
-              <option className={styles.sort_option} value="highter">By highter price </option>
-              <option className={styles.sort_option} value="lower">By lower price</option>
-            </select>
+            <SortProducts sortProducts={sortProducts} />
           </span>
         </div>
 
       </div>
 
       <div className={styles.products_container}>
-
-        {products.map((product) => {
-
-          return <div key={product.id} className={styles.product_container}>
-            <div >
-              <img className={styles.img_secateurs} src={product.imgSrc} alt="" />
-            </div>
-            <div className={styles.product_description}>
-              <div className={styles.container_price}>
-                <p className={styles.new_price}>{product.newPrice}</p>
-                <p className={styles.old_price}>{product.oldPrice}</p>
-                <p className={styles.discount}>{product.discount}</p>
-              </div>
-              <p className={styles.name_product}>{product.name}</p>
-            </div>
-          </div>
-
-        })}
-
-
-
+        <ViewProducts products={products} />
       </div>
     </div>
   )
 }
 
-
-{/* <div className={styles.tools_container}>
-      <p className={styles.rubric}>All products</p>
-
-      <div className={styles.form_container}>
-
-        <div className={styles.price_container}>
-          <span className={styles.text}>Price</span>
-          <input placeholder='from' className={styles.price_input}></input>
-          <input placeholder='to' className={styles.price_input}></input>
-        </div>
-
-        <div className={styles.discount_container}>
-          <span className={styles.text}>Discounted items</span>
-          <span>
-            <input type='checkbox' className={styles.discount_checkbox}></input>
-          </span>
-        </div>
-
-        <div className={styles.sort_container}>
-          <span className={styles.text}>Sorted</span>
-          <span>
-            <select className={styles.sort_select}>
-              <option className={styles.sort_option} value="default">By default</option>
-              <option className={styles.sort_option} value="highter">By highter price </option>
-              <option className={styles.sort_option} value="lower">By lower price</option>
-            </select>
-          </span>
-        </div>
-
-      </div>
-
-      <div className={styles.products_container}>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_secateurs}>
-            <img src="" alt="" />
-          </div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>Secateurs</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_collection_for_berries}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>Collection for berries (plastic)</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_gloves}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>Gloves (black)</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_hacksaw}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>sickle-shaped hacksaw</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_shovel}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>bayonet shovel</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_pitchfork}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>garden pitchfork</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_barbell}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>Barbell</p>
-          </div>
-        </div>
-
-        <div className={styles.product_container}>
-          <div className={styles.img_thermometer}></div>
-          <div className={styles.product_description}>
-            <div className={styles.container_price}>
-              <p className={styles.new_price}>199$</p>
-              <p className={styles.old_price}>250$</p>
-              <p className={styles.discount}>-7%</p>
-            </div>
-            <p className={styles.name_product}>souvenir thermometer</p>
-          </div>
-        </div>
-
-      </div>
-    </div> */}
